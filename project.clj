e37078031a4af7ce21211560247f40a99d80e599
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:57:33 +0200

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iloveponies.tests/sudoku "0.1.0-SNAPSHOT"
+  :description "tests for sudoku"
+  :url "http://github.com/iloveponies/sudoku"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]
+                 [midje-grader "0.1.0-SNAPSHOT"]])
