d83a0092f4c196f2c684e4985e53493e4d572901
Ilmo Raunio <ilmo.raunio@gmail.com>
2019-06-24 18:35:14 +0300

    Add support for latest jdk (11)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject iloveponies.tests/sudoku "0.1.0-SNAPSHOT"
+(defproject iloveponies.tests/sudoku "0.2.0-SNAPSHOT"
   :description "tests for sudoku"
   :url "http://github.com/iloveponies/sudoku"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [midje "1.9.4"]
                  [midje-grader "0.1.0-SNAPSHOT"]])
